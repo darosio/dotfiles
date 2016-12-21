d52e69f690ca27c78f86f4a0b0895a5ba93932cf
daniele arosio <daniele.arosio@cnr.it>
2016-12-21 15:38:54 +0100

    updated 2016-12-21

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -306,11 +306,12 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+ (setq-default org-directory "~/Sync/share/phone/box/notes/")
  (setq-default dotspacemacs-configuration-layers
    '((org :variables org-projectile-file "~/Sync/share/phone/box/TODOs.org")))
- (setq org-agenda-files (quote ("~/Sync/share/phone/box/todo.org")))
+ (setq org-agenda-files (quote ("~/Sync/share/phone/box/notes/todo.org")))
  (setq org-capture-templates
-   '(("t" "todo" entry (file+headline "~/Sync/share/phone/box/todo.org" "Tasks")
+   '(("t" "todo" entry (file+headline "~/Sync/share/phone/box/notes/todo.org" "Tasks")
       "* TODO [#A] %?")))
  ;; my personal config
  (push "~/.spacemacs.d/config/" load-path)
@@ -328,6 +329,9 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Sync/share/phone/box/notes/spacemacs.org" "~/Sync/share/phone/box/notes/todo.org")))
  '(package-selected-packages
    (quote
     (yapfify pyvenv pytest pyenv-mode py-isort pip-requirements live-py-mode hy-mode helm-pydoc cython-mode anaconda-mode pythonic yaml-mode csv-mode web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode helm-css-scss haml-mode emmet-mode flyspell-correct-helm flyspell-correct auto-dictionary mmm-mode markdown-toc markdown-mode gh-md spray mu4e-maildirs-extension mu4e-alert ht org-projectile org-present org org-pomodoro alert log4e gntp org-download htmlize gnuplot ws-butler window-numbering which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spacemacs-theme spaceline restart-emacs request rainbow-delimiters quelpa popwin persp-mode pcre2el paradox org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide ido-vertical-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav dumb-jump define-word column-enforce-mode clean-aindent-mode auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line))))
