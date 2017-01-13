9f25d5a6f61679e5000cd5d72b425aa7324b363f
daniele arosio <daniele.arosio@cnr.it>
2017-01-13 08:18:27 +0100

    updated 2017-01-13

--- spacemacs/.spacemacs.d/config/mu4e-config.el
+++ spacemacs/.spacemacs.d/config/mu4e-config.el
@@ -16,6 +16,7 @@
 ;; allow for updating mail using 'U' in the main view:
 ;(setq mu4e-get-mail-command "offlineimap -u quiet")
 (setq mu4e-get-mail-command "mbsync cnr")
+;(setq mu4e-update-interval 120)
 ;;rename files when moving
 ;;NEEDED FOR MBSYNC
 (setq mu4e-change-filenames-when-moving t)
@@ -26,19 +27,18 @@
          ("/cnr/Templates"    . ?t)
          ("/cnr/Drafts"       . ?a)))
 ;; something about ourselves
-(setq
- mu4e-reply-to-address "daniele.arosio@cnr.it"
- user-mail-address "daniele.arosio@cnr.it"
+; mu4e-reply-to-address "daniele.arosio@cnr.it"
+(setq user-mail-address "daniele.arosio@cnr.it"
  user-full-name  "Daniele Arosio"
  message-signature
  (concat
   "Daniele Arosio\n"
-  "Institute of Biophysics"
-  "National Research Council of Italy"
-  "Bruno Kessler Foundation"
-  "Via Sommarive 18"
-  "38123 Trento, Italy"
-  "Email: daniele.arosio@cnr.it\n"
+  "Institute of Biophysics\n"
+  "National Research Council of Italy\n"
+  "Bruno Kessler Foundation\n"
+  "Via Sommarive 18\n"
+  "38123 Trento, Italy\n"
+  "Email: daniele.arosio@cnr.it"
   "\n")) ;(setq message-signature-file "~/.emacs.d/.signature") ;
 
 (setq send-mail-function (quote sendmail-send-it)
@@ -56,10 +56,9 @@
 ;; from http://www.macs.hw.ac.uk/~rs46/posts/2014-01-13-mu4e-email-client.html
 ; get mail
 ;(setq mu4e-get-mail-command "mbsync -c ~/.emacs.d/.mbsyncrc work"
-(setq mu4e-html2text-command "w3m -T text/html"
-      mu4e-update-interval 120
-      mu4e-headers-auto-update t
-      mu4e-compose-signature-auto-include nil)
+;(setq mu4e-html2text-command "w3m -T text/html")
+      ;mu4e-headers-auto-update t
+      ;mu4e-compose-signature-auto-include nil)
 
 ;; show images
 (setq mu4e-show-images t)
@@ -72,10 +71,10 @@
 ; (setq mu4e-sent-messages-behavior 'delete)
 
 ;; spell check
-(add-hook 'mu4e-compose-mode-hook
-        (defun my-do-compose-stuff ()
-           "My settings for message composition."
-           (set-fill-column 72)
-           (flyspell-mode)))
+;(add-hook 'mu4e-compose-mode-hook
+        ;(defun my-do-compose-stuff ()
+           ;"My settings for message composition."
+           ;(set-fill-column 72)
+           ;(flyspell-mode)))
 
 (provide 'mu4e-config)
