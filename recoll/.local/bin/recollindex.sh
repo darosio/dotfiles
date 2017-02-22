4ce7b3c3f96a86c5758ac22c7624a0e8d08c138a
daniele arosio <daniele.arosio@cnr.it>
2017-02-22 12:21:47 +0100

    updated 2017-02-22

--- recoll/.local/bin/recollindex.sh
+++ recoll/.local/bin/recollindex.sh
@@ -4,7 +4,6 @@ echo "To: daniele.arosio@cnr.it" > /tmp/recoll.out
 echo "From: $HOSTNAME" >> /tmp/recoll.out
 echo "Subject: recollindex from $HOSTNAME" >> /tmp/recoll.out
 cat ~/.recoll/idxstatus.txt >> /tmp/recoll.out
-/usr/bin/recollindex >> /tmp/recoll.out 2>&1
+time /usr/bin/recollindex >> /tmp/recoll.out 2>&1
 cat ~/.recoll/idxstatus.txt >> /tmp/recoll.out
-#cat /tmp/recoll.out | mail -s recollindex danielepietroarosio@gmail.com
 cat /tmp/recoll.out | msmtp daniele.arosio@cnr.it
