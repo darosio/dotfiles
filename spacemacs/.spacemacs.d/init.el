4815c2d7d1cfed5395924aa8df3ce276b2d01384
daniele arosio <daniele.arosio@cnr.it>
2017-05-10 19:00:44 +0200

    updated 2017-05-10

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -366,14 +366,27 @@ you should place your code here."
    (add-hook 'org-mode-hook (lambda () (org-zotxt-mode 1)))
    ;; Change citation format to be less cumbersome in files.
    ;; You'll need to install mkbehr-short into your style manager first.
-   (eval-after-load "zotxt"
-     '(setq zotxt-default-bibliography-style "zookeys"))
+   ;; (eval-after-load "zotxt"
+   ;;   '(setq zotxt-default-bibliography-style "citekey"))
    (setq reftex-default-bibliography '("~/Sync/bib.bib"))
 
+   (setq bibtex-completion-pdf-field "file")
+   ;; (defun my/org-ref-open-pdf-at-point ()
+   ;;   "Open the pdf for bibtex key under point if it exists."
+   ;;   (interactive)
+   ;;   (let* ((results (org-ref-get-bibtex-key-and-file))
+   ;;          (key (car results))
+   ;;          (pdf-file (car (bibtex-completion-find-pdf key))))
+   ;;     (if (file-exists-p pdf-file)
+   ;;         (org-open-file pdf-file)
+   ;;       (message "No PDF found for %s" key))))
+   ;; (setq org-ref-open-pdf-function 'my/org-ref-open-pdf-at-point)
+
    ;; see org-ref for use of these variables
-   (setq org-ref-bibliography-notes "~/Sync/bibliography/notes.org"
+   (setq org-ref-bibliography-notes "~/Sync/media/bibliography.org"
          org-ref-default-bibliography '("~/Sync/bib.bib")
-         ;; org-ref-pdf-directory "~/Sync/media/zotfiles/"
+         org-ref-completion-pdf-field "File"
+         ;; org-ref-pdf-directory "~/Sync/media/zotfiles/Journal Article"
          )
 
 )
