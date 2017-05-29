4ee9d1ed108183a85f1295f5244224949bacfae9
daniele arosio <daniele.arosio@cnr.it>
2017-05-29 19:11:11 +0200

    updated 2017-05-29

--- spacemacs/.spacemacs.d/config/publish-config.el
+++ spacemacs/.spacemacs.d/config/publish-config.el
@@ -9,19 +9,21 @@
       `(("metaModels-notes"
          ;; Publish details
          :base-directory "~/metamodels/"
-         :exclude-tags '("noexport" "abstract")
+         :exclude-tags ("noexport" "abstract")
          :base-extension "org"
          :recursive t
          ;; :exclude (regexp-opt "themes" "cv" "README.org")
          :publishing-function org-html-publish-to-html
          ;; :html-postamble pank.eu-postamble
          :publishing-directory "~/public"
+         :html-head-extra "<link rel="stylesheet" type="text/css" href="http://thomasf.github.io/solarized-css/solarized-light.min.css" />"
          ;; Org fiddling
-         :html-head-extra "<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/main.css\">"
+         ;; :html-head-extra "<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.css\">"
          ;; html fiddling
          ;; :html-preamble (pank.eu-preamble nil "PhD candidate in economics")
          ;; :sitemap-filename "index.org"
-         :table-of-contents t)
+         :table-of-contents t
+         :toc 3)
 
         ("metaModels-static"
          :base-directory "~/metamodels/"
@@ -33,12 +35,15 @@
         ("css"
          :base-directory "~/metamodels/css/"
          :base-extension "css"
+         :recursive t
          :publishing-directory "~/public/css/"
          :publishing-function org-publish-attachment)
 
         ("metaModels"
          :components ("metaModels-notes" "metaModels-static" "css")
          :auto-sitemap t
+         ;; #+INFOJS_OPT: view:t toc:t ltoc:t mouse:underline buttons:0 path:http://thomasf.github.io/solarized-css/org-info.min.js
+         ;; :infojs_opt "view:t toc:t ltoc:t mouse:underline buttons:0 path:http://thomasf.github.io/solarized-css/org-info.min.js"
          ))
       )
 
