66a4f658bf7f47ef384b1327faf447d6018b202d
daniele arosio <daniele.arosio@cnr.it>
2017-05-30 21:20:25 +0200

    updated 2017-05-30

--- spacemacs/.spacemacs.d/config/publish-config.el
+++ spacemacs/.spacemacs.d/config/publish-config.el
@@ -1,14 +1,14 @@
 (require 'ox-publish)
 
 (setq org-html-html5-fancy t
-      org-html-htmlize-output-type 'css
+      ;; org-html-htmlize-output-type 'css
       org-html-head-include-default-style t
       )
 
 (setq org-publish-project-alist
       `(("metaModels-notes"
          ;; Publish details
-         :base-directory "~/metamodels/"
+         :base-directory "~/workspace/arte/Web/metamodels/"
          :exclude-tags ("noexport" "abstract")
          :base-extension "org"
          :recursive t
@@ -16,31 +16,28 @@
          :publishing-function org-html-publish-to-html
          ;; :html-postamble pank.eu-postamble
          :publishing-directory "~/public"
-         :html-head-extra "<link rel="stylesheet" type="text/css" href="http://thomasf.github.io/solarized-css/solarized-light.min.css" />"
+         ;; :html-head-extra "<link rel="stylesheet" type="text/css" href="http://thomasf.github.io/solarized-css/solarized-light.min.css">"
+         ;; :html-head-extra "<link rel=\"stylesheet\" type=\"text/css\" href=\"./myfile.css\">"
          ;; Org fiddling
          ;; :html-head-extra "<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.css\">"
          ;; html fiddling
          ;; :html-preamble (pank.eu-preamble nil "PhD candidate in economics")
          ;; :sitemap-filename "index.org"
-         :table-of-contents t
-         :toc 3)
+         :headline-levels 4             ;; Just the default for this project.
+         ;; :table-of-contents t
+         ;; :toc 3
+         :auto-preamble t
+         )
 
-        ("metaModels-static"
-         :base-directory "~/metamodels/"
-         :base-extension "jpg\\|gif\\|png"
-         :publishing-directory "~/public"
-         :recursive t
-         :publishing-function org-publish-attachment)
-
-        ("css"
-         :base-directory "~/metamodels/css/"
-         :base-extension "css"
-         :recursive t
-         :publishing-directory "~/public/css/"
-         :publishing-function org-publish-attachment)
+         ("metaModels-static"
+          :base-directory "~/workspace/arte/Web/metamodels/"
+          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+          :publishing-directory "~/public"
+          :recursive t
+          :publishing-function org-publish-attachment)
 
         ("metaModels"
-         :components ("metaModels-notes" "metaModels-static" "css")
+         :components ("metaModels-notes" "metaModels-static")
          :auto-sitemap t
          ;; #+INFOJS_OPT: view:t toc:t ltoc:t mouse:underline buttons:0 path:http://thomasf.github.io/solarized-css/org-info.min.js
          ;; :infojs_opt "view:t toc:t ltoc:t mouse:underline buttons:0 path:http://thomasf.github.io/solarized-css/org-info.min.js"
@@ -48,4 +45,3 @@
       )
 
 (provide 'publish-config)
-:html-head-extra "<link rel=\"stylesheet\" type=\"text/css\" href=\"./myfile.css\">"
