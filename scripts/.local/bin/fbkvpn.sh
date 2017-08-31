2b121dafaee842877703bd555ca04f65b2eddf7a
daniele arosio <daniele.arosio@cnr.it>
2017-08-31 12:59:42 +0200

    updated 2017-08-31

---
+++ scripts/.local/bin/fbkvpn.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+(pass email/fbk | sudo openconnect --juniper sslgate.fbk.eu -u darosio --passwd-on-stdin) &
+pid=$!
+while ping -c1 vigolana > /dev/null 2>&1;
+do
+	sleep 2
+done
+
+echo "Process ID: " $pid
+sudo ip route del 10.0.0.0/16 dev tun0
+wait $pid
