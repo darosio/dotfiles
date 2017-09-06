a25be3824aecc2cee04b69688d9eef1e00eaf1f2
daniele arosio <daniele.arosio@cnr.it>
2017-09-06 18:46:33 +0200

    updated 2017-09-06

--- spacemacs/.spacemacs.d/config/elfeed-config.el
+++ spacemacs/.spacemacs.d/config/elfeed-config.el
@@ -1,38 +1,15 @@
 (require 'elfeed)
 (require 'elfeed-goodies)
 (elfeed-goodies/setup)
-
+(setq httpd-port 8181)
 ;; (setq elfeed-search-trailing-width 23)
 ;; (setq elfeed-search-title-max-width 190)
 ;; (defun elfeed-search-format-date (date)
 ;;   (format-time-string "%Y-%m-%d %H:%M" (seconds-to-time date)))
 
-;; probably temporary: hack for elfeed-goodies date column:
-(defun elfeed-goodies/search-header-draw 
-  "Returns the string to be used as the Elfeed header."
-  (if (zerop (elfeed-db-last-update))
-      (elfeed-search--intro-header)
-    (let* ((separator-left (intern (format "powerline-%s-%s"
-        elfeed-goodies/powerline-default-separator
-        (car powerline-default-separator-dir))))
-    (separator-right (intern (format "powerline-%s-%s"
-         elfeed-goodies/powerline-default-separator
-         (cdr powerline-default-separator-dir))))
-    (db-time (seconds-to-time (elfeed-db-last-update)))
-    (stats (-elfeed/feed-stats))
-    (search-filter (cond
-      (elfeed-search-filter-active
-       "")
-      (elfeed-search-filter
-       elfeed-search-filter)
-      (""))))
-      (if (>= (window-width) (* (frame-width) elfeed-goodies/wide-threshold))
-   (search-header/draw-wide separator-left separator-right search-filter stats db-time)
- (search-header/draw-tight separator-left separator-right search-filter stats db-time)))))
 
 (defun elfeed-goodies/entry-line-draw (entry)
   "Print ENTRY to the buffer."
-
   (let* ((title (or (elfeed-meta entry :title) (elfeed-entry-title entry) ""))
   (date (elfeed-search-format-date (elfeed-entry-date entry)))
   (title-faces (elfeed-search--faces (elfeed-entry-tags entry)))
@@ -60,14 +37,13 @@
       elfeed-goodies/feed-source-column-width
       elfeed-goodies/feed-source-column-width)
          :left)))
-
     (if (>= (window-width) (* (frame-width) elfeed-goodies/wide-threshold))
  (progn
    (insert (propertize date 'face 'elfeed-search-date-face) " ")
    (insert (propertize feed-column 'face 'elfeed-search-feed-face) " ")
    (insert (propertize tag-column 'face 'elfeed-search-tag-face) " ")
    (insert (propertize title 'face title-faces 'kbd-help title)))
-      (insert (propertize title 'face title-faces 'kbd-help title)))))
+    (insert (propertize title 'face title-faces 'kbd-help title)))))
 
 
 ;; http://pragmaticemacs.com/emacs/read-your-rss-feeds-in-emacs-with-elfeed/
@@ -87,16 +63,12 @@
   (elfeed-db-save)
   (quit-window))
 
-
 ;; http://pragmaticemacs.com/category/elfeed/
 (defalias 'elfeed-toggle-star
   (elfeed-expose #'elfeed-search-toggle-all 'star))
-(eval-after-load 'elfeed-search
-  '(define-key elfeed-search-mode-map (kbd "f") 'elfeed-toggle-star))
-;; (define-key elfeed-search-mode-map (kbd "f") 'elfeed-toggle-star)
+;; (eval-after-load 'elfeed-search
+;;   '(define-key elfeed-search-mode-map (kbd "f") 'elfeed-toggle-star))
 (define-key elfeed-show-mode-map (kbd ".") 'visual-fill-column-mode)
-
-(setq httpd-port 8181)
 
 
 ;; Hydra macros and elfeed tags
@@ -146,20 +118,21 @@
 (define-key elfeed-search-mode-map (kbd "<DEL>") 'elfeed-search-untag-all-unread)
 (define-key elfeed-search-mode-map (kbd "J") 'mz/make-and-run-elfeed-hydra)
 
-(defface python-elfeed-entry
+;; Custom faces
+(defface pYthon-elfeed-entry
   '((t :background "Darkseagreen1"))
   "Marks a python Elfeed entry.")
-
 (defface emacs-elfeed-entry
-  '((t :background "Lightblue1"))
-  "Marks a python Elfeed entry.")
+  '((t :background "Blue"))
+  "Marks a emacs Elfeed entry.")
+(defface elfeed-search-starred-title-face
+  '((t :foreground "Orange"))
+  "Marks a starred Elfeed entry.")
+(push '(python python-elfeed-entry) elfeed-search-face-alist)
+(push '(emacs emacs-elfeed-entry) elfeed-search-face-alist)
+(push '(starred elfeed-search-starred-title-face) elfeed-search-face-alist)
 
-(push '(python python-elfeed-entry)
-      elfeed-search-face-alist)
-
-(push '(emacs emacs-elfeed-entry)
-      elfeed-search-face-alist)
-
+;; Captures, email, doi into biblio
 (defun email-elfeed-entry ()
   "Capture the elfeed entry and put it in an email."
   (interactive)
@@ -270,9 +243,8 @@
 				      (bibtex-beginning-of-entry)
 				      (delete-char -2)))))))))))))
 
-
 (define-key elfeed-show-mode-map (kbd "e") 'email-elfeed-entry)
-(define-key elfeed-show-mode-map (kbd "c") (lambda () (interactive) (org-capture nil "e")))
+(define-key elfeed-show-mode-map (kbd "c") (lambda () (interactive) (org-capture nil "t")))
 (define-key elfeed-show-mode-map (kbd "d") 'doi-utils-add-entry-from-elfeed-entry)
 
 (provide 'elfeed-config)
