e9980688ed605cac6e499a80971b7b2934de2d08
daniele arosio <daniele.arosio@cnr.it>
2018-02-05 12:51:06 +0100

    updated 2018-02-05

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -398,6 +398,11 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  ;; (with-eval-after-load 'helm
+  ;;   (setq helm-display-function 'helm-default-display-buffer))
+  (setq helm-display-function 'helm-display-buffer-in-own-frame
+        helm-display-buffer-reuse-frame t
+        helm-use-undecorated-frame-option t)
   ;; C-x C-0 restores the default font size
   (global-set-key (kbd "C-+") 'text-scale-increase)
   (global-set-key (kbd "C--") 'text-scale-decrease)
