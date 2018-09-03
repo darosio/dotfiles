d5c97a4ee33f617ae787187bd36311eb2911d619
daniele arosio <daniele.arosio@cnr.it>
2018-09-03 16:53:52 +0200

    updated 2018-09-03

--- spacemacs/.spacemacs.d/config/my-org-publish.el
+++ spacemacs/.spacemacs.d/config/my-org-publish.el
@@ -1,3 +1,5 @@
+(provide 'my-org-publish)
+
 ;; activate koma scripts export
 (eval-after-load 'ox '(require 'ox-koma-letter))
 
@@ -6,23 +8,21 @@
 
 (setq org-latex-pdf-process
       '("latexmk -pdflatex='pdflatex -interaction nonstopmode' -pdf -bibtex -f %f"))
-;; '("xelatex -interaction nonstopmode %f"
-;;   "xelatex -interaction nonstopmode %f")
-(require 'ox-latex)
-(add-to-list 'org-latex-classes
-             '("koma-article"
-               "\\documentclass{scrartcl}
-                \\usepackage{microtype}
-                \\usepackage{tgtermes}
-                \\usepackage[scale=.9]{tgheros}
-                \\usepackage{tgcursor}
-                \\usepackage{paralist}
-                \\newcommand{\\rc}{$^{14}C$}"
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-(require 'ox-beamer)
+;; ;;   "xelatex -interaction nonstopmode %f"
 
-(provide 'my-org-publish)
+;; (require 'ox-latex)
+;; (add-to-list 'org-latex-classes
+;;              '("koma-article"
+;;                "\\documentclass{scrartcl}
+;;                 \\usepackage{microtype}
+;;                 \\usepackage{tgtermes}
+;;                 \\usepackage[scale=.9]{tgheros}
+;;                 \\usepackage{tgcursor}
+;;                 \\usepackage{paralist}
+;;                 \\newcommand{\\rc}{$^{14}C$}"
+;;                ("\\section{%s}" . "\\section*{%s}")
+;;                ("\\subsection{%s}" . "\\subsection*{%s}")
+;;                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+;;                ("\\paragraph{%s}" . "\\paragraph*{%s}")
+;;                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+;; (require 'ox-beamer)
