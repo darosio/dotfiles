e859f8be9b8662e9ff311b5449ea952bb19e19b6
daniele arosio <daniele.arosio@cnr.it>
2019-08-24 00:30:16 +0200

    improved borg.sh script; to be tested.

--- borg/.local/bin/borg.sh
+++ borg/.local/bin/borg.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
 #
-out=/tmp/`basename "$0"`-`date +%d%H%M%S`
+out=/tmp/$(basename "$0")-$(date +%d%H%M%S)
 REPOSITORY=/data/borg
 SRC=/data/Sync
+thishost=$(uname -n)
 
-echo "To: daniele.arosio@cnr.it" 				 > $out
-echo "From: $HOSTNAME" 						>> $out
-echo "Subject: borg backup on $HOSTNAME"			>> $out
+{
+    echo "To: daniele.arosio@cnr.it"
+    echo "From: $thishost"
+    echo "Subject: borg backup on $thishost"
+    echo "Backing up .."
+    borg create --info --stats -C zlib --list \
+         "$REPOSITORY::dan-$(date +%Y-%m-%d)" $SRC \
+         --exclude $SRC/tmp --exclude '*/.stversions' --exclude '*.pyc'
+        # --exclude '*/Sync/tmp' \
+    echo "Pruning archives .."
+    # --prefix dan- is very important to limit prune's operation to this
+    # machine's archives and not apply to other machine's archives also.
+    borg prune --info --list --stats $REPOSITORY --prefix dan- \
+	       --keep-daily=4 --keep-weekly=2 --keep-monthly=6 --keep-yearly=10
+} > "$out" 2>&1
 
-printf "\nBackup...\n" 						>> $out
-#	--exclude $SRC/tmp
-borg create --info --stats -C zlib    --list        \
-    $REPOSITORY::dan-`date +%Y-%m-%d`               \
-    $SRC	--exclude '*/tmp'                   \
-		--exclude '*/.stversions'   	    \
-		--exclude '*.pyc' 				>> $out 2>&1
 
-printf "\nPruning archives...\n"				>> $out
-# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
-# archives of THIS machine. --prefix `hostname`- is very important to
-# limit prune's operation to this machine's archives and not apply to
-# other machine's archives also.
-#borg prune -v $REPOSITORY --prefix `hostname`- \
-borg prune --info --list --stats $REPOSITORY --prefix dan- \
-	--keep-daily=4 --keep-weekly=2 --keep-monthly=6    \
-	--keep-yearly=10 					>> $out 2>&1
-
-cat $out | grep -v "/*/" | msmtp daniele.arosio@cnr.it
+# cat $out | grep -v "/*/" | msmtp daniele.arosio@cnr.it
+cat < "$out" | msmtp daniele.arosio@cnr.it
