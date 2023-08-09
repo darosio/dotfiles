d152200d2ca0a5984fcff02d658897e75b4dd623
daniele arosio <daniele.arosio@cnr.it>
2023-08-09 18:38:48 +0200

    feat: Fix recoll.stow

--- recoll.stow.sh
+++ recoll.stow.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 #
+yay -S --noconfirm recoll
+yay -S --noconfirm pstotext
+yay -S --noconfirm python-mutagen
+yay -S --noconfirm python-lxml
+yay -S --noconfirm python-pychm
 mkdir -p "$HOME"/.recoll
 mkdir -p "$HOME"/.config/systemd/user
 mkdir -p "$HOME"/.local/bin
 stow -t "$HOME" recoll
+pacaur -Qi recoll
+yay -S --noconfirm python-unrar
+yay -S --noconfirm lyx
