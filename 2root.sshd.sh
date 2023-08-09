0107b944c4c9398b7b6d8b4860817c23a3c6238a
daniele arosio <daniele.arosio@cnr.it>
2023-08-09 12:46:56 +0200

    refactor: mr.stow

--- 2root.sshd.sh
+++ 2root.sshd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
-yay -S openssh
+yay -S --noconfirm openssh
 sudo mkdir -p /etc/systemd/system/sshd.socket.d/
 sudo stow -t / 2root.sshd
 sudo systemctl enable sshd.service
--- mr.stow.sh
+++ mr.stow.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 #
+yay -S --noconfirm myrepos
 mkdir -p "$HOME"/workspace/arte
 mkdir -p "$HOME"/workspace/examples
+sudo mkdir -p /home/dati
+sudo chown dan:dan /home/dati
 stow -t /home mr
