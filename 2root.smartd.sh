3a6d599727976de67f3b4a30972f22db7e8eaf88
daniele arosio <daniele.arosio@cnr.it>
2023-08-10 15:16:37 +0200

    fix: 2root.smartd.sh

--- 2root.smartd.sh
+++ 2root.smartd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
-yay -S --noconfirm smartmontools
 sudo stow -t / 2root.smartd
+yay -S --noconfirm smartmontools
 sudo systemctl enable smartd.service
 sudo systemctl start smartd.service
