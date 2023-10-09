a522580847fce7cdcbbcca3b5f0b176c7c57104f
daniele arosio <daniele.arosio@cnr.it>
2023-10-09 18:58:31 +0200

    refactor: Add script also for RAID essential into root

---
+++ 2root.raid_mdadm_scrubbing.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+#
+sudo stow -t / 2root.raid_mdadm_scrubbing
+if ! yay -Q mdadm >/dev/null 2>&1; then
+    yay -S --noconfirm mdadm
+fi
+# DEVICE partitions
+sudo mdadm --examine --scan | sudo tee -a /etc/mdadm.conf
+echo "MAILADDR root" | sudo tee -a /etc/mdadm.conf
+sudo mdadm --monitor --scan --oneshot --test
+f
+sudo systemctl start raid_scrubbing.timer
+sudo systemctl enable raid_scrubbing.timer
