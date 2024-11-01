c16632852d526d6d40acb0a09f076fc90e8ad7d8
daniele arosio <daniele.arosio@cnr.it>
2024-11-01 17:15:30 +0100

    fix: nftables

--- 2root.nftables.sh
+++ 2root.nftables.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 #
 yay -S --noconfirm nftables
+sudo rm /etc/nftables.conf
 sudo stow -t / 2root.nftables
-sudo systemctl enable nftables.service 
-sudo systemctl start nftables.service 
+sudo systemctl enable nftables.service
+sudo systemctl start nftables.service
