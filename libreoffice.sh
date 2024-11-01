7d1f378ed9e4c614d8af0a91471ab5006ae33be9
daniele arosio <daniele.arosio@cnr.it>
2024-11-01 21:57:03 +0100

    feat: Add libreoffice.sh

---
+++ libreoffice.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+#
+yay -S --noconfirm hyphen
+yay -S --noconfirm hyphen-en
+yay -S --noconfirm hyphen-it
+yay -S --noconfirm libwpg
+yay -S --noconfirm hunspell
+yay -S --noconfirm hunspell-en_us
+yay -S --noconfirm hunspell-it
+yay -S --noconfirm pstoedit
+yay -S --noconfirm libmythes
+yay -S --noconfirm mythes-en
+yay -S --noconfirm mythes-it
+yay -S --noconfirm libreoffice-fresh
+yay -S --noconfirm libreoffice-extension-languagetool # AUR
