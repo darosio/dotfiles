3ca8fb5ced06a16195162b664d3c47bb7a6051c0
daniele arosio <daniele.arosio@cnr.it>
2025-04-03 16:32:39 +0200

    fix: mu

--- mu.stow.sh
+++ mu.stow.sh
@@ -6,8 +6,16 @@ yay -S --noconfirm isync
 stow -t "$HOME" mbsync
 # yay -S --noconfirm mb2md
 
-mkdir -p "$HOME"/Maildir/
+mkdir -p "$HOME"/Maildir/gmail
+mkdir -p "$HOME"/Maildir/pec
+mbsync -a
+
 mu init --maildir "$HOME"/Maildir/ --my-address=daniele.arosio@cnr.it \
   --my-address danielepietroarosio@gmail.com \
   --my-address daniele.arosio@postecert.it
 mu index
+
+cd "$HOME"/Maildir || exit
+ln -sf "$HOME"/Sync/Maildir/archive .
+ln -sf "$HOME"/Sync/Maildir/personal .
+ln -sf "$HOME"/Sync/Maildir/mailrc .
