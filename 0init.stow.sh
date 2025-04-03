e618e67b6f30d2d3e66fe6752de0bf817946d99c
daniele arosio <daniele.arosio@cnr.it>
2025-04-03 12:44:18 +0200

    fix: 0init.stow.sh

--- 0init.stow.sh
+++ 0init.stow.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 #
 mkdir -p "$HOME"/.config
+rm -rf "$HOME"/.bashrc
+rm -rf "$HOME"/.bash_profile
+rm -rf "$HOME"/.bash_logout
 stow -t "$HOME" bash
 stow -t "$HOME" misc
 stow -t "$HOME" scripts
 
 yay -S --noconfirm python-hatch
-yay -S --noconfirm pyenv-virtualenv
-yay -S --noconfirm direnv
+#yay -S --noconfirm pyenv-virtualenv
+#yay -S --noconfirm direnv
+
+
