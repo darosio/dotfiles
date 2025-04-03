caadfe80e1d3095520b3ddaf69928ef64b66e95b
daniele arosio <daniele.arosio@cnr.it>
2025-04-03 13:30:35 +0200

    fix: stow into /etc/systemd/system

--- 2root.systemd.sh
+++ 2root.systemd.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 #
 sudo mkdir -p /etc/systemd/system/service.d
+sudo mkdir -p /etc/systemd/system/failure-notification@.service.d
 sudo stow -t / 2root.systemd
 sudo mkdir -p /etc/systemd/system/failure-notification@.service.d
 sudo touch /etc/systemd/system/failure-notification@.service.d/toplevel-override.conf
