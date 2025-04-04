4821d6072c06c873cfe7f0367a44385ff9f471a1
daniele arosio <daniele.arosio@cnr.it>
2025-04-04 15:09:40 +0200

    fix: psd and firefox

--- psd_firefox.stow.sh
+++ psd_firefox.stow.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 #
+yay -S --noconfirm firefox
 yay -S --noconfirm profile-sync-daemon
 mkdir -p "$HOME"/.config/psd
 stow -t "$HOME" psd
+
+systemctl --user enable --now psd.service
