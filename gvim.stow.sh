72f262d9c2da8a2e67c9eb75f7ce08bed7c5059b
daniele arosio <daniele.arosio@cnr.it>
2025-04-15 16:08:12 +0200

    fix: gvim

--- gvim.stow.sh
+++ gvim.stow.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env sh
 #
+rm -rf "$HOME"/.vim
+
 yay -S --noconfirm gvim
 mkdir -p "$HOME"/.vim/bundle
 stow -t "$HOME" gvim
+
+# Store the current directory
+ORIGINAL_DIR=$(pwd)
+cd "$HOME"/.vim || exit
+[ -d "$HOME"/Sync/.vim ] && ln -sf "$HOME"/Sync/.vim/* .
+# Return to the original directory
+cd "$ORIGINAL_DIR" || exit
 
 git clone https://github.com/VundleVim/Vundle.vim.git "$HOME"/.vim/bundle/Vundle.vim
 # Open Vim and install plugins using vim-plug
