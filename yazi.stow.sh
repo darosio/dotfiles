d0d268ab684f60ab32229a61cea19935bbb6d792
daniele Arosio <daniele.arosio@cnr.it>
2025-07-30 23:24:42 +0200

    feat(yazi): office plugin

---
+++ yazi.stow.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+#
+# Check if the script is already running in an isolated environment
+if [ -z "$ISOLATED_ENV" ]; then
+    # Re-run the script in an isolated environment without 'exec' so it continues
+    env -i HOME="$HOME" USER="$USER" PATH="/usr/bin:$PATH" ISOLATED_ENV=1 "$0" "$@"
+    exit # Ensure we don't run the original environment after re-execution
+fi
+# Clear any lingering variables that might interfere
+unset PYTHONPATH
+
+yay -S --noconfirm yazi
+
+yay -S --noconfirm imv
+yay -S --noconfirm bat
+yay -S --noconfirm eza
+yay -S --noconfirm ffmpegthumbnailer
+yay -S --noconfirm highlight
+yay -S --noconfirm imagemagick
+yay -S --noconfirm mediainfo
+yay -S --noconfirm odt2txt
+yay -S --noconfirm perl-image-exiftool
+yay -S --noconfirm poppler
+yay -S --noconfirm python-pillow
+yay -S --noconfirm transmission-cli
+yay -S --noconfirm trash-cli
+
+mkdir -p "$HOME"/.bashrc.d
+mkdir -p "$HOME"/.config/yazi
+mkdir -p "$HOME"/.local/share/applications
+stow -t "$HOME" yazi
+
+yay -S --noconfirm perl-xls2csv
+# yay -S --noconfirm python-pipx
+# pipx install xlsx2csv
+uv tool install xlsx2csv
