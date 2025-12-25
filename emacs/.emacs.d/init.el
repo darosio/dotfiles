20927131c0cf6779ffd2d80d977e29dc808d90c3
daniele Arosio <daniele.arosio@cnr.it>
2025-12-25 08:12:39 +0100

    fix(emacs): ensure nil for recentf built-in package

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -46,12 +46,12 @@
 
 ;; --- Package Management (Elpaca) ---
 
-(defvar elpaca-installer-version 0.10)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil
+                              :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
@@ -59,25 +59,31 @@
        (order (cdr elpaca-order))
        (default-directory repo))
   (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
+  (unless (file-exists-p (expand-file-name "elpaca.el" repo))
+    (when (file-exists-p repo) (delete-directory repo t))
     (make-directory repo t)
-    (when (< emacs-major-version 28) (require 'subr-x))
+    (when (<= emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
-        (if-let ((buffer (url-retrieve-synchronously
-                          "https://raw.githubusercontent.com/progfolio/elpaca/master/installer.el" t)))
-            (unwind-protect
-                (with-current-buffer buffer
-                  (goto-char (point-max))
-                  (eval-print-last-sexp))
-              (kill-buffer buffer))
-          (error "Hook provider (url-retrieve-synchronously) failed"))
-      (error
-       (delete-directory elpaca-directory t)
-       (error (format "Could not install Elpaca: %s" err) err))))
-  (when (require 'elpaca nil t)
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load (expand-file-name "elpaca-autoloads" repo) nil t)
-    (elpaca-process-queues)))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
+(add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
 ;; Install use-package support
@@ -135,6 +141,7 @@
     (ediff-diff-options "-w"))
 
   (use-package emacs
+    :ensure nil
     :preface
     ;; Go to change fonts
     (defun mk-set-font (font &optional height)
@@ -426,6 +433,7 @@
     (html-mode . aggressive-indent-mode))
 
   (use-package delsel
+    :ensure nil
     :init (delete-selection-mode 1))
 
   (use-package fix-word
@@ -503,6 +511,7 @@
     (move-text-default-bindings))  ;; Binds M-<up>/<down> automatically
 
   (use-package recentf
+    :ensure nil
     :init (recentf-mode t))
 
   (use-package transpose-frame
@@ -714,6 +723,7 @@
 
   ;; Emacs minibuffer tuning
   (use-package emacs
+    :ensure nil
     :custom
     (enable-recursive-minibuffers t)
     (read-extended-command-predicate #'command-completion-default-include-p) ;only commands working in current mode
