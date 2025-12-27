e6e952d0c60c3fcec51c192136a6b35774fc910b
daniele Arosio <daniele.arosio@cnr.it>
2025-12-27 09:25:01 +0100

    feat: add wifite and sniffnet

---
+++ wifite.stow.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+yay -S --noconfirm wifite
+yay -S --noconfirm reaver
+yay -S --noconfirm termshark
+yay -S --noconfirm hashcat
+yay -S --noconfirm hcxdumptool
+yay -S --noconfirm hcxtools
+yay -S --noconfirm bully
+yay -S --noconfirm cowpatty
+
+yay -S --noconfirm sniffnet
