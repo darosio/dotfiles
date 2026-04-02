a59caea29bb102bb327582ced1670207fff401e5
daniele Arosio <daniele.arosio@cnr.it>
2026-04-02 15:57:05 +0200

    fix: msmtp for root

--- 2root.msmtp.sh
+++ 2root.msmtp.sh
@@ -2,4 +2,11 @@
 #
 yay -S --noconfirm msmtp
 yay -S --noconfirm msmtp-mta
+sudo rm /etc/msmtprc /etc/msmtp.aliases
 sudo stow -t / 2root.msmtp
+
+# Create plaintext password file for root msmtp (used at boot without GPG agent)
+if [ ! -f /home/dan/.msmtp-gmail-pass ]; then
+  PASSWORD_STORE_DIR=/home/dan/Sync/.pass/ pass email/gmail-isync | head -1 > /home/dan/.msmtp-gmail-pass
+  chmod 600 /home/dan/.msmtp-gmail-pass
+fi
