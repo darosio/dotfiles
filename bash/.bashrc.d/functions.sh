aaeba8b3e6ebe836afc09e1e4b092657e7b8ff6b
daniele Arosio <daniele.arosio@cnr.it>
2026-06-06 07:18:50 +0200

    feat: add claude_deepseek

--- bash/.bashrc.d/functions.sh
+++ bash/.bashrc.d/functions.sh
@@ -287,3 +287,16 @@ tree_size() {
   level="$1"
   find . -maxdepth "$level" -type d -exec bash -c 'depth=$(echo "$1" | awk -F/ "{print NF-1}"); indent=$(printf "%$((depth*4))s" ""); size=$(du -sh "$1" 2>/dev/null | awk "{print \$1}"); name=$(basename "$1"); echo -e "${indent}├── [ ${size:-    }]  ${name}";' _ {} \; | sed 's/├/│/g; s/└/├/g; $!N;s/│   /│   /; s/├/└/; s/\[ \]/[    ]/'
 }
+
+claude_deepseek() {
+  export ANTHROPIC_BASE_URL=https://api.deepseek.com/anthropic
+  ANTHROPIC_AUTH_TOKEN=$(pass show cloud/deepseek)
+  export ANTHROPIC_AUTH_TOKEN
+  export ANTHROPIC_MODEL="deepseek-v4-pro[1m]"
+  export ANTHROPIC_DEFAULT_OPUS_MODEL="deepseek-v4-pro[1m]"
+  export ANTHROPIC_DEFAULT_SONNET_MODEL="deepseek-v4-pro[1m]"
+  export ANTHROPIC_DEFAULT_HAIKU_MODEL=deepseek-v4-flash
+  export CLAUDE_CODE_SUBAGENT_MODEL=deepseek-v4-flash
+  export CLAUDE_CODE_EFFORT_LEVEL=max
+  claude
+}
