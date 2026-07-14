0c44e0b22a721fcbd80340510d409b814bd4fcc6
daniele Arosio <daniele.arosio@cnr.it>
2026-07-14 16:40:48 +0200

    fix: syncthing

--- syncthing.stow.client.sh
+++ syncthing.stow.client.sh
@@ -2,7 +2,8 @@
 #
 yay -S --noconfirm syncthing
 mkdir -p "$HOME"/Sync
-stow -t "$HOME" syncthing
+#stow -t "$HOME" syncthing
+cp syncthing/Sync/.stignore "$HOME"/Sync/
 
 systemctl enable --user syncthing.service
 systemctl start --user syncthing.service
